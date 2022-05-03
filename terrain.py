6170ce6dad1942e8f077895667aa85c7d58a9e6d
Rubal Ravinder <rubal.ravinder@gmail.com>
2022-05-03 12:32:43 +0200

    generate terrain v0

---
+++ terrain.py
@@ -0,0 +1,21 @@
+class Terrain:
+    def __init__(self, h_w, x_center, y_center):
+        self.h_w = h_w
+        self.x_center = x_center
+        self.y_center = y_center
+
+    def generate_terrain(self):
+        """
+        Generate a 2D array with a radial gradient
+        """
+        x = np.linspace(-10, 10, self.h_w)
+        y = np.linspace(-10, 10, self.h_w)
+        x, y = np.meshgrid(x, y)
+        terrain = np.sqrt((x-self.x_center)**2+(y-self.y_center)**2)
+        return self.terrain
+
+    def plot_terrain(self):
+        """
+        Plots in colorbar the 2D array
+        """
+        plt.imshow(self.terrain)
